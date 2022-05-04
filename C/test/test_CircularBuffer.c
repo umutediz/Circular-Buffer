1ec4fb8a974da01fd34e564d17f3d2fd89fdf344
Umut Ediz <umut@ediz.dev>
2022-05-04 19:13:17 +0300

    Formatting of testbench

--- C/test/test_CircularBuffer.c
+++ C/test/test_CircularBuffer.c
@@ -1,166 +1,167 @@
-#include "unity.h"
-#include "stdlib.h"
-#include "stdio.h"
-#include "string.h"
 #include "CircularBuffer.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "unity.h"
 
 #define BUFFER_SIZE 64
 tCircularBuffer* cb;
 
-void setUp(void)
-{
-	cb = malloc(sizeof(tCircularBuffer));
-	cb->Buffer = (uint8_t*) malloc(BUFFER_SIZE);
-	memset((uint8_t*)cb->Buffer, 0, BUFFER_SIZE);
-	cb->size = BUFFER_SIZE;
-	cb->readPos = 0;
-	cb->writePos = 0;
+void setUp(void) {
+  cb = malloc(sizeof(tCircularBuffer));
+  cb->Buffer = (uint8_t*)malloc(BUFFER_SIZE);
+  memset((uint8_t*)cb->Buffer, 0, BUFFER_SIZE);
+  cb->size = BUFFER_SIZE;
+  cb->readPos = 0;
+  cb->writePos = 0;
 }
 
-void tearDown(void)
-{
-	free((uint8_t*)cb->Buffer);
-	free((tCircularBuffer*)cb);
+void tearDown(void) {
+  free((uint8_t*)cb->Buffer);
+  free((tCircularBuffer*)cb);
 }
 
-void test_cb_single_write(){
-	writeCircularBuffer(cb, 0x55);
-	TEST_ASSERT_EQUAL_UINT8(0x55, cb->Buffer[0]);
-	TEST_ASSERT_EQUAL_UINT16(1, cb->writePos);
+void test_cb_single_write() {
+  writeCircularBuffer(cb, 0x55);
+  TEST_ASSERT_EQUAL_UINT8(0x55, cb->Buffer[0]);
+  TEST_ASSERT_EQUAL_UINT16(1, cb->writePos);
 }
 
-void test_cb_single_read(){
-	cb->Buffer[0] = 0x55;
-	cb->writePos=1;
+void test_cb_single_read() {
+  cb->Buffer[0] = 0x55;
+  cb->writePos = 1;
 
-	uint8_t data;
-	TEST_ASSERT_EQUAL_UINT16(0, cb->readPos);
-	TEST_ASSERT_EQUAL_UINT16(1, bufferLength(cb));
-	TEST_ASSERT(readCircularBuffer(cb, &data) == BufferTrue);
-	TEST_ASSERT_EQUAL_UINT8(0x55, data);
-	TEST_ASSERT_EQUAL_UINT16(1, cb->readPos);
+  uint8_t data;
+  TEST_ASSERT_EQUAL_UINT16(0, cb->readPos);
+  TEST_ASSERT_EQUAL_UINT16(1, bufferLength(cb));
+  TEST_ASSERT(readCircularBuffer(cb, &data) == BufferTrue);
+  TEST_ASSERT_EQUAL_UINT8(0x55, data);
+  TEST_ASSERT_EQUAL_UINT16(1, cb->readPos);
 }
 
-void test_buffer_length(){
-	#define COUNT 5
-	for (uint8_t i = 0; i<COUNT; i++){
-		writeCircularBuffer(cb, 0x55);
-	}
-	TEST_ASSERT_EQUAL_UINT16(COUNT, bufferLength(cb));
+void test_buffer_length() {
+#define COUNT 5
+  for (uint8_t i = 0; i < COUNT; i++) {
+    writeCircularBuffer(cb, 0x55);
+  }
+  TEST_ASSERT_EQUAL_UINT16(COUNT, bufferLength(cb));
 }
 
-void test_buffer_length_overflow(){
-	#define COUNT 5
+void test_buffer_length_overflow() {
+#define COUNT 5
 
-	for (uint8_t i = 0; i<BUFFER_SIZE-1; i++){
-		writeCircularBuffer(cb, 0x55);
-	}
-	TEST_ASSERT_EQUAL_UINT16(BUFFER_SIZE-1, bufferLength(cb));
+  for (uint8_t i = 0; i < BUFFER_SIZE - 1; i++) {
+    writeCircularBuffer(cb, 0x55);
+  }
+  TEST_ASSERT_EQUAL_UINT16(BUFFER_SIZE - 1, bufferLength(cb));
 
-	uint8_t data;
-	for (uint8_t i = 0; i<COUNT*2; i++){
-		readCircularBuffer(cb, &data);
-	}
+  uint8_t data;
+  for (uint8_t i = 0; i < COUNT * 2; i++) {
+    readCircularBuffer(cb, &data);
+  }
 
-	for (uint8_t i = 0; i<COUNT; i++){
-		writeCircularBuffer(cb, 0x55);
-	}
+  for (uint8_t i = 0; i < COUNT; i++) {
+    writeCircularBuffer(cb, 0x55);
+  }
 
-	TEST_ASSERT_EQUAL_UINT16(58, bufferLength(cb));
+  TEST_ASSERT_EQUAL_UINT16(58, bufferLength(cb));
 }
 
-void test_cb_write_sequence(){
-	uint8_t sequence[] = "This is a test string";
-	TEST_ASSERT_EQUAL_UINT8(22, sizeof(sequence));
-	for(uint8_t i=0; i<sizeof(sequence); i++){
-		writeCircularBuffer(cb, sequence[i]);
-	}
+void test_cb_write_sequence() {
+  uint8_t sequence[] = "This is a test string";
+  TEST_ASSERT_EQUAL_UINT8(22, sizeof(sequence));
+  for (uint8_t i = 0; i < sizeof(sequence); i++) {
+    writeCircularBuffer(cb, sequence[i]);
+  }
 
-	TEST_ASSERT_EQUAL_UINT8(sizeof(sequence), cb->writePos);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY(sequence, cb->Buffer, sizeof(sequence));
+  TEST_ASSERT_EQUAL_UINT8(sizeof(sequence), cb->writePos);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(sequence, cb->Buffer, sizeof(sequence));
 }
 
-void test_cb_read_sequence(){
-	uint8_t actual_data[] = "This is a test string";
-	memcpy((uint8_t*)cb->Buffer, actual_data, sizeof(actual_data));
-	cb->writePos=sizeof(actual_data);
+void test_cb_read_sequence() {
+  uint8_t actual_data[] = "This is a test string";
+  memcpy((uint8_t*)cb->Buffer, actual_data, sizeof(actual_data));
+  cb->writePos = sizeof(actual_data);
 
-	TEST_ASSERT_EQUAL_UINT8_ARRAY(actual_data, cb->Buffer, sizeof(actual_data));
-	TEST_ASSERT_EQUAL_UINT16(0, cb->readPos);
-	uint8_t sequence[22];
-	for(uint8_t i=0; i<sizeof(sequence); i++){
-		readCircularBuffer(cb, sequence+i);
-	}
-	TEST_ASSERT_EQUAL_UINT16(sizeof(actual_data), cb->readPos);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY(actual_data, sequence, sizeof(actual_data));
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(actual_data, cb->Buffer, sizeof(actual_data));
+  TEST_ASSERT_EQUAL_UINT16(0, cb->readPos);
+  uint8_t sequence[22];
+  for (uint8_t i = 0; i < sizeof(sequence); i++) {
+    readCircularBuffer(cb, sequence + i);
+  }
+  TEST_ASSERT_EQUAL_UINT16(sizeof(actual_data), cb->readPos);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(actual_data, sequence, sizeof(actual_data));
 }
 
-void test_cb_write_sequence_overflow(){
-	uint8_t sequence[BUFFER_SIZE];
+void test_cb_write_sequence_overflow() {
+  uint8_t sequence[BUFFER_SIZE];
 
-	for(uint32_t i=0; i<sizeof(sequence)-1; i++){
-		sequence[i] = i;
-		tBufferStatus status = writeCircularBuffer(cb, sequence[i]);
-		uint8_t message[256];
-		if (status != BufferTrue){
-			sprintf(message, "Write Position of circular buffer is %d at %dth iteration", cb->writePos, i);
-			TEST_ASSERT_MESSAGE( status == BufferTrue, message);
-		}
-	}
+  for (uint32_t i = 0; i < sizeof(sequence) - 1; i++) {
+    sequence[i] = i;
+    tBufferStatus status = writeCircularBuffer(cb, sequence[i]);
+    uint8_t message[256];
+    if (status != BufferTrue) {
+      sprintf(message,
+              "Write Position of circular buffer is %d at %dth iteration",
+              cb->writePos, i);
+      TEST_ASSERT_MESSAGE(status == BufferTrue, message);
+    }
+  }
 
-	TEST_ASSERT_EQUAL_UINT16(cb->size-1, cb->writePos);
-	TEST_ASSERT(writeCircularBuffer(cb, 0x55) == BufferFull);
-	TEST_ASSERT_EQUAL_UINT16(sizeof(sequence)-1, cb->writePos);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY(sequence, cb->Buffer, sizeof(sequence));
+  TEST_ASSERT_EQUAL_UINT16(cb->size - 1, cb->writePos);
+  TEST_ASSERT(writeCircularBuffer(cb, 0x55) == BufferFull);
+  TEST_ASSERT_EQUAL_UINT16(sizeof(sequence) - 1, cb->writePos);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(sequence, cb->Buffer, sizeof(sequence));
 }
 
-void test_cb_chasing_read(){
-	uint8_t data;
-	tBufferStatus status;
+void test_cb_chasing_read() {
+  uint8_t data;
+  tBufferStatus status;
 
-	for(uint32_t i=0; i<BUFFER_SIZE/4; i++){
-		status = writeCircularBuffer(cb, 0x55);
-		TEST_ASSERT(status == BufferTrue);
-	}
-	TEST_ASSERT_EQUAL_UINT16(BUFFER_SIZE/4, cb->writePos);
+  for (uint32_t i = 0; i < BUFFER_SIZE / 4; i++) {
+    status = writeCircularBuffer(cb, 0x55);
+    TEST_ASSERT(status == BufferTrue);
+  }
+  TEST_ASSERT_EQUAL_UINT16(BUFFER_SIZE / 4, cb->writePos);
 
-	for(uint32_t i=0; i<BUFFER_SIZE/4; i++){
-		status = readCircularBuffer(cb, &data);
-		TEST_ASSERT(status == BufferTrue);
-	}
+  for (uint32_t i = 0; i < BUFFER_SIZE / 4; i++) {
+    status = readCircularBuffer(cb, &data);
+    TEST_ASSERT(status == BufferTrue);
+  }
 
-	status = readCircularBuffer(cb, &data);
-	TEST_ASSERT(status == BufferEmpty);
+  status = readCircularBuffer(cb, &data);
+  TEST_ASSERT(status == BufferEmpty);
 }
 
-void test_cb_chasing_read_overflow(){
-	uint8_t data;
-	tBufferStatus status;
+void test_cb_chasing_read_overflow() {
+  uint8_t data;
+  tBufferStatus status;
 
-	for(uint32_t i=0; i<BUFFER_SIZE/4; i++){
-		status = writeCircularBuffer(cb, 0x55);
-		TEST_ASSERT(status == BufferTrue);
-	}
-	TEST_ASSERT_EQUAL_UINT16(BUFFER_SIZE/4, cb->writePos);
+  for (uint32_t i = 0; i < BUFFER_SIZE / 4; i++) {
+    status = writeCircularBuffer(cb, 0x55);
+    TEST_ASSERT(status == BufferTrue);
+  }
+  TEST_ASSERT_EQUAL_UINT16(BUFFER_SIZE / 4, cb->writePos);
 
-	for(uint32_t i=0; i<BUFFER_SIZE/4; i++){
-		status = readCircularBuffer(cb, &data);
-		TEST_ASSERT(status == BufferTrue);
-	}
+  for (uint32_t i = 0; i < BUFFER_SIZE / 4; i++) {
+    status = readCircularBuffer(cb, &data);
+    TEST_ASSERT(status == BufferTrue);
+  }
 
-	for(uint32_t i=0; i<BUFFER_SIZE-1; i++){
-		status = writeCircularBuffer(cb, 0x55);
-		TEST_ASSERT(status == BufferTrue);
-	}
-	TEST_ASSERT(cb->writePos < cb->readPos); //make sure overflow occured
-	TEST_ASSERT(bufferLength(cb) > 0);
+  for (uint32_t i = 0; i < BUFFER_SIZE - 1; i++) {
+    status = writeCircularBuffer(cb, 0x55);
+    TEST_ASSERT(status == BufferTrue);
+  }
+  TEST_ASSERT(cb->writePos < cb->readPos);  // make sure overflow occured
+  TEST_ASSERT(bufferLength(cb) > 0);
 
-	uint32_t read_count = bufferLength(cb);
-	for (uint32_t i = 0; i < read_count; i++){
-		status = readCircularBuffer(cb, &data);
-		TEST_ASSERT(status == BufferTrue);
-	}
+  uint32_t read_count = bufferLength(cb);
+  for (uint32_t i = 0; i < read_count; i++) {
+    status = readCircularBuffer(cb, &data);
+    TEST_ASSERT(status == BufferTrue);
+  }
 
-	TEST_ASSERT_EQUAL_UINT32_MESSAGE(0, bufferLength(cb), "Buffer still contains data");
-	TEST_ASSERT(readCircularBuffer(cb, &data) == BufferEmpty);
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(0, bufferLength(cb),
+                                   "Buffer still contains data");
+  TEST_ASSERT(readCircularBuffer(cb, &data) == BufferEmpty);
 }
